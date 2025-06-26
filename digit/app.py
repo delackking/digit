6c16b23a02cb262b1ebd95b255ffff9d8b897d99
Ansh Raj <delackking@gmail.com>
2025-06-26 20:32:36 +0530

    Update app.py

--- digit/app.py
+++ digit/app.py
@@ -30,7 +30,7 @@ def predict():
     prediction = model.predict(processed)[0]
     return jsonify({'prediction': int(prediction)})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
